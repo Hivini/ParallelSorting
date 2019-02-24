22d099860b058ca83ce9f399c50bb00e125587ff
Hivini <vinicioquinterobuss@gmail.com>
2019-02-23 22:44:23 -0600

    First commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+/**
+ * @author Jorge Vinicio Quintero Santos
+ * @className Main
+ * @date Feb/23/2019
+ * @comments None
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
